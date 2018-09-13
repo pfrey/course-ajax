00f7a526a6c9ad0677741c8442ca34633228e385
Patty <patty.fresonke@forumcomm.com>
2018-09-13 15:46:07 -0500

    Section 4, Lesson 5: Ajax with XHR

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,59 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = function (err) {
+            requestError(err, 'image');
+        };
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID a7c9abcd19cf5625b3f7461ff7b7a45da6524090f7a8394f5771178f2dea650b');
+        unsplashRequest.send();
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+            requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=341b5abb3a6c4d0d8b4587f48c02177a`);
+        articleRequest.send();
     });
+
+
+
+    function addImage(){
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+
+      if (data && data.results && data.results[0]) {
+        const firstImage = data.results[0];
+        htmlContent = `<figure>
+          <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+          <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+      } else {
+        htmlContent = '<div class="error-no-image">No images available</div>';
+      }
+
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
 })();
